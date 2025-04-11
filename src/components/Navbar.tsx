a8be423898988c799e0886ee52c05ef63572a990
Ishu <kumarishu494@gmail.com>
2025-04-11 09:48:44 +0530

    Navbar implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,148 @@
 "use client";
 
-import { Sun, Moon } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
+import {
+  Sun,
+  Moon,
+  Bell,
+  Calendar,
+  Smile,
+  Calculator,
+  User,
+  CreditCard,
+  Settings,
+  Search,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+import {
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandShortcut,
+  CommandSeparator,
+} from "@/components/ui/command";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Link from "next/link";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setSearchOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   return (
     <div className="w-full flex items-center justify-between px-8 py-4 sticky top-0 border-b">
-      <div>Search here</div>
-      <div className="flex gap-8">
+      {/* Search bar */}
+      <Button
+        variant="outline"
+        className="w-80 flex justify-between text-sm hover:bg-accent text-muted-foreground hover:text-muted-foreground px-4 py-2"
+        onClick={() => setSearchOpen(true)}
+      >
+        <span className="flex gap-2 items-center">
+          <Search />
+          Search here{" "}
+        </span>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog open={searchOpen} onOpenChange={setSearchOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <Calendar />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <Smile />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <Calculator />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+
+      <div className="flex gap-4">
+        {/* Notification */}
+        <Drawer direction="right">
+          <DrawerTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Bell className="h-[1.2rem] w-[1.2rem]" />
+              <span className="sr-only">Notification</span>
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Notifications</DrawerTitle>
+              <DrawerDescription>
+                You can save around 40% on your food expenses
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <Button>Clear all</Button>
+              <DrawerClose asChild>
+                <Button variant="outline">Close</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+
+        {/* Theme Toggle */}
         <Button
-          variant="outline"
+          variant="ghost"
           size="icon"
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         >
@@ -19,7 +150,50 @@ export default function Navbar() {
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
-        <div>Profile</div>
+
+        {/* Avatar */}
+        <Popover>
+          <PopoverTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="Avatar" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div className="grid gap-4">
+              <div className="flex flex-col items-center space-y-2">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="Avatar"
+                    className="w-[5rem]"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <h4 className="font-medium leading-none">Ishu Coder</h4>
+                <p className="text-sm text-muted-foreground">
+                  email@example.com
+                </p>
+              </div>
+              <div className="flex justify-between">
+                {[
+                  {
+                    title: "Edit profile",
+                    url: "/settings/profile",
+                    variant: "outline",
+                  },
+                  { title: "Logout", url: "/logout", variant: "default" },
+                ].map((option) => (
+                  // <div className="flex items-center gap-4" key={option.title}>
+                  <Button variant={option.variant} key={option.title}>
+                    <Link href={option.url}>{option.title}</Link>
+                  </Button>
+                  // </div>
+                ))}
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
