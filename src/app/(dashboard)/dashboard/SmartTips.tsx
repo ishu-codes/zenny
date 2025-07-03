e30b6f43905e0c9d84bd445e1d63c44f6c94ad02
Ishu <kumarishu494@gmail.com>
2025-07-03 12:06:20 +0530

    Smart tips: Swipeable implemented

--- src/app/(dashboard)/dashboard/SmartTips.tsx
+++ src/app/(dashboard)/dashboard/SmartTips.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import {
   Card,
   CardHeader,
@@ -13,31 +15,62 @@ import { useState } from "react";
 
 export default function SmartTips() {
   const [index, setIndex] = useState<number>(0);
-  const increment = () => {
-    setIndex(index + 1 >= SMART_TIPS.length ? 0 : index + 1);
+  const [swiperInstance, setSwiperInstance] = useState<any>(null);
+
+  const handlePrevTip = () => {
+    const newIndex = index > 0 ? index - 1 : SMART_TIPS.length - 1;
+    swiperInstance?.slideTo(newIndex);
   };
-  const decrement = () => {
-    setIndex(index - 1 < 0 ? SMART_TIPS.length - 1 : index - 1);
+  const handleNextTip = () => {
+    const newIndex = index < SMART_TIPS.length - 1 ? index + 1 : 0;
+    swiperInstance?.slideTo(newIndex);
   };
+  const moveTo = (index: number) => {
+    swiperInstance?.slideTo(index);
+  };
+
   return (
     <Card className="overflow-x-hidden">
       <CardHeader>
         <CardTitle>Smart Tips</CardTitle>
       </CardHeader>
-      <CardContent className="flex gap-6 min-h-20 relative">
-        {SMART_TIPS?.map((tip, idx) => (
-          <div
-            key={idx}
-            className="absolute inset-0 w-full space-y-2 px-6 transition-all duration-300"
-            style={{ left: `${(idx - index) * 100}%` }}
-          >
-            <h3 className="text-md">{tip?.title}</h3>
-            <p className="text-muted-foreground text-sm">{tip?.desc}</p>
+      <CardContent className="flex gap-6 min-h-20 relative px-0">
+        <div className="flex whitespace-nowrap overflow-hidden">
+          <div className="w-full flex gap-4 transition-transform duration-300 ease-in-out">
+            <Swiper
+              modules={[Navigation, Pagination]}
+              spaceBetween={20}
+              slidesPerView={1}
+              onSwiper={setSwiperInstance}
+              onSlideChange={(e) => setIndex(e.activeIndex)}
+              className="cursor-grab active:cursor-grabbing"
+            >
+              {SMART_TIPS?.map((tip, idx) => (
+                <SwiperSlide key={idx}>
+                  <div
+                    key={idx}
+                    className="w-full shrink-0 relative text-white overflow-hidden"
+                    // style={{ left: `${(idx - index) * 100}%` }}
+                  >
+                    <div className="flex flex-col gap-2 whitespace-normal px-6">
+                      <h3 className="text-md text-foreground">{tip?.title}</h3>
+                      <p className="text-muted-foreground text-sm">
+                        {tip?.desc}
+                      </p>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
-        ))}
+        </div>
       </CardContent>
-      <CardFooter className="flex items-center justify-between">
-        <Button variant={"outline"} onClick={decrement}>
+      <CardFooter className="flex items-center justify-center md:justify-between">
+        <Button
+          className="hidden md:block"
+          variant={"outline"}
+          onClick={handlePrevTip}
+        >
           Previous
         </Button>
 
@@ -52,11 +85,13 @@ export default function SmartTips() {
               className={`h-2 rounded-full transition-all duration-300 ease-in-out ${
                 idx === index ? "w-10 bg-primary" : "w-2 bg-primary/20"
               }`}
-              onClick={() => setIndex(idx)}
+              onClick={() => moveTo(idx)}
             />
           ))}
         </div>
-        <Button onClick={increment}>Next</Button>
+        <Button className="hidden md:block" onClick={handleNextTip}>
+          Next
+        </Button>
       </CardFooter>
     </Card>
   );
